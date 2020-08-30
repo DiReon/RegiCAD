d9fa5f0837ea5323e154f35d290da3712133d164
Dmitry Gorbachev <direon16@gmail.com>
2020-08-30 22:41:55 +0400

    Added automatic testing. Also fixed bug with filename update in the browser. Removed all console logging

--- regicad.js
+++ regicad.js
@@ -5,7 +5,6 @@ var Variable = /** @class */ (function () {
     }
     Object.defineProperty(Variable.prototype, "result", {
         get: function () {
-            console.log("this.exp: ", this.exp);
             return regiRound(evaluate(this.exp), precision);
         },
         enumerable: true,
@@ -16,26 +15,21 @@ var Variable = /** @class */ (function () {
 var evaluate = function (str) {
     var varsNames = vars.map(function (x) { return x.name; });
     str = replaceByStars(str);
-    console.log('String to evaluate: ', str); //Here need to brake string into array and check for every array element
     var strArr = str.replace(/ |;/gi, '').split(/[\+\-\**\*\/\(\)]/gi);
-    console.log("Array to evaluate: ", strArr);
     for (var i = 0; i < strArr.length + 1; i++) {
         var index = varsNames.indexOf(strArr[i]);
-        console.log("Index: ", index, "i: ", i, "srtArr.length: ", strArr.length);
         if (index != -1) {
             str = str.replace(strArr[i], "(" + vars[index].exp + ")").replace(/ /gi, '');
             strArr = str.replace(/[\(\)]/gi, '').split(/[\+\-\**\*\/)\(\)]/gi);
-            console.log("Expression: " + str + ", strArr: " + strArr);
             i = -1;
         }
     }
-    console.log("String before eval: ", str, " precision: ", precision, "result: ", eval(str));
     return regiRound(eval(str), precision);
 };
 var createVariable = function (str) {
     str = replaceByStars(str);
-    var name = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[0];
-    var exp = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[1];
+    var name = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[0].replace(/|(<*>)/gi, '');
+    var exp = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[1].replace(/|(<*>)/gi, '');
     var varsNames = vars.map(function (x) { return x.name; });
     if (varsNames.includes(name)) {
         var index = varsNames.indexOf(name);
@@ -43,7 +37,6 @@ var createVariable = function (str) {
     }
     var v = new Variable(name, exp);
     vars.push(v);
-    console.log("Vars: " + vars.map(function (v) { return v.name + '=' + v.exp + '=' + v.result; }));
     return v;
 };
 var calculate = function () {
@@ -58,18 +51,17 @@ var calculate = function () {
     for (var line in temp) {
         textElement.innerHTML += "<div>" + temp[line] + "</div>";
     }
+    save();
 };
 var processLine = function (line) {
     if (line.indexOf('=') === -1)
         return line;
     if (line.replace(/;|&nbsp/gi, '').includes('<b>=</b>')) {
-        console.log("This is variable declaration...:", line);
         var v = createVariable(line);
         return v.name + " <b>=</b> " + replaceStars(v.exp);
     }
     else {
         var lineArr = line.split('=');
-        console.log('This is equation, lineArr[0]:', lineArr[0]);
         return replaceStars(lineArr[0]) + " = " + evaluate(lineArr[0]);
     }
 };
@@ -93,21 +85,18 @@ var replaceStars = function (str) {
     return str.replace(/\*\*/gi, '^').replace(/\*/gi, '∙');
 };
 var save = function () {
-    var newFilename = document.getElementById('filename').value;
-    localStorage.setItem(newFilename, textElement.innerHTML);
-    if (filelist.indexOf(newFilename) === -1) {
-        filelist.unshift(newFilename);
+    localStorage.setItem(newFilename.value, textElement.innerHTML);
+    if (filelist.indexOf(newFilename.value) === -1) {
+        filelist.unshift(newFilename.value);
         localStorage.setItem('RegiCADfiles', filelist.join());
     }
     updateFileList();
 };
 var deleteFile = function () {
     var filename = selectFile.value;
-    console.log("selectFile.value: ", selectFile.value);
     localStorage.removeItem(filename);
     var index = filelist.indexOf(filename);
     filelist.splice(index, 1);
-    console.log("Filelist: ", filelist);
     localStorage.removeItem('RegiCADfiles');
     localStorage.setItem('RegiCADfiles', filelist.join());
     updateFileList();
@@ -123,22 +112,24 @@ var updateFileList = function () {
 };
 var loadFile = function () {
     textElement.innerHTML = localStorage.getItem(selectFile.value);
-    document.getElementById('filename').value = selectFile.value;
+    newFilename.value = selectFile.value;
 };
-var vars = [];
-var filelist = [];
-var regiFiles = localStorage.getItem('RegiCADfiles');
-if (regiFiles)
-    filelist = regiFiles.split(',');
 var textElement = document.getElementById('content');
 var precisionEl = document.getElementById('precision');
 var saveBtn = document.getElementById('saveBtn');
 var calcBtn = document.getElementById('calcBtn');
 var deleteBtn = document.getElementById('deleteBtn');
 var selectFile = document.getElementById('selectFile');
+var newFilename = document.getElementById('filename');
+var vars = [];
+var filelist = [];
+var regiFiles = localStorage.getItem('RegiCADfiles');
+if (regiFiles)
+    filelist = regiFiles.split(',');
 var lastFile = filelist[0];
 if (lastFile) {
     textElement.innerHTML = localStorage.getItem(lastFile);
+    newFilename.value = lastFile;
 }
 updateFileList();
 var precision = +precisionEl.value;
@@ -151,3 +142,30 @@ precisionEl.addEventListener('change', calculate);
 saveBtn.addEventListener('click', save);
 deleteBtn.addEventListener('click', deleteFile);
 selectFile.addEventListener('click', loadFile);
+//Testing.....
+var testArr = [
+    "<div>1+2=</div>",
+    "<div>2*3=</div>",
+    "<div>(1+2)^3-(4*5)/6=</div>",
+    "<div>abc<b>=</b>12.3</div><div>def<b>=</b>45.6</div><div>abc+def=</div>",
+    "<div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет депрессии при открытии скважины Хазри-2 объект 4</div><div>ρ&nbsp;<b>=</b>&nbsp;1.2</div><div>Глубина верха интервала перфорации:</div><div>Интервал_верх&nbsp;<b>=</b>&nbsp;4407</div><div><br></div><div>Давление гидростатики на глубине верха интервала:</div><div>Ph&nbsp;<b>=</b>&nbsp;ρ*Интервал_верх*1.423</div><div>Ph = 7525</div><div><br></div><div>Пластовое давление:</div><div>Pf&nbsp;<b>=</b>&nbsp;47.24*145</div><div>Pf = 6850</div><div><br></div><div>После замещения необходимо стравить давление на устье для депрессии 10 МПа:</div><div>Ph - Pf + 10*145 = 2126</div><div>В МПа:</div><div>(Ph - Pf)/145 + 10 = 14.7</div><div><br></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет объема замещения</div><div>Внутренний объем НКТ на 1 м:</div><div>0.063**2*0.785 = 0</div><div>Объем вытеснения при замещении на 300 м выше IRDV:</div><div>4177-300 = 3877</div><div>3877*0.003115 = 12.1</div><div>0.5*0.55 = 0.28</div>",
+    "<div></div><div></div><div></div><div></div><div></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Volume calculations Khazri-2 zone 4</div><div>Liner_ID <b>=</b> 0.1524</div><div>Casing1_ID <b>=</b> 0.2262</div><div>Casing2_ID <b>=</b> 0.2168</div><div>Casing3_ID <b>=</b> 0.219</div><div>Packer_depth <b>=</b> 4778</div><div>Tubing_OD <b>=</b> 0.0889</div><div>Tubing_ID <b>=</b> 0.063</div><div>Well_volume <b>=</b> 0.785∙(Casing1_ID^2∙1098.19+Casing2_ID^2∙(3143.26-1098.19)+Casing3_ID^2∙(4221.24-3143.26)+Liner_ID^2∙(Packer_depth-4221.24))</div><div>Well_volume     = 170.3</div><div>Annulus_volume <b>=</b> Well_volume-0.785∙Tubing_OD^2∙Packer_depth</div><div>Annulus_volume    = 140.7</div><div>Tubing_volume <b>=</b> 0.785∙Tubing_ID^2∙Packer_depth</div><div>Tubing_volume    = 14.89</div><div><br></div>"
+];
+var resultsArr = [
+    "<div></div><div>1+2 = 3</div>",
+    "<div></div><div>2∙3 = 6</div>",
+    "<div></div><div>(1+2)^3-(4∙5)/6 = 23.67</div>",
+    "<div></div><div>abc <b>=</b> 12.3</div><div>def <b>=</b> 45.6</div><div>abc+def = 57.9</div>",
+    "<div></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет депрессии при открытии скважины Хазри-2 объект 4</div><div>ρ <b>=</b> 1.2</div><div>Глубина верха интервала перфорации:</div><div>Интервал_верх <b>=</b> 4407</div><div><br></div><div>Давление гидростатики на глубине верха интервала:</div><div>Ph <b>=</b> ρ∙Интервал_верх∙1.423</div><div>Ph  = 7525</div><div><br></div><div>Пластовое давление:</div><div>Pf <b>=</b> 47.24∙145</div><div>Pf  = 6850</div><div><br></div><div>После замещения необходимо стравить давление на устье для депрессии 10 МПа:</div><div>Ph - Pf + 10∙145  = 2126</div><div>В МПа:</div><div>(Ph - Pf)/145 + 10  = 14.66</div><div><br></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет объема замещения</div><div>Внутренний объем НКТ на 1 м:</div><div>0.063^2∙0.785  = 0.003116</div><div>Объем вытеснения при замещении на 300 м выше IRDV:</div><div>4177-300  = 3877</div><div>3877∙0.003115  = 12.08</div><div>0.5∙0.55  = 0.275</div>",
+    "<div></div><div></div><div></div><div></div><div></div><div></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Volume calculations Khazri-2 zone 4</div><div>Liner_ID <b>=</b> 0.1524</div><div>Casing1_ID <b>=</b> 0.2262</div><div>Casing2_ID <b>=</b> 0.2168</div><div>Casing3_ID <b>=</b> 0.219</div><div>Packer_depth <b>=</b> 4778</div><div>Tubing_OD <b>=</b> 0.0889</div><div>Tubing_ID <b>=</b> 0.063</div><div>Well_volume <b>=</b> 0.785∙(Casing1_ID^2∙1098.19+Casing2_ID^2∙(3143.26-1098.19)+Casing3_ID^2∙(4221.24-3143.26)+Liner_ID^2∙(Packer_depth-4221.24))</div><div>Well_volume      = 170.3</div><div>Annulus_volume <b>=</b> Well_volume-0.785∙Tubing_OD^2∙Packer_depth</div><div>Annulus_volume     = 140.7</div><div>Tubing_volume <b>=</b> 0.785∙Tubing_ID^2∙Packer_depth</div><div>Tubing_volume     = 14.89</div><div><br></div>"
+];
+for (var key in testArr) {
+    textElement.innerHTML = testArr[key];
+    calculate();
+    textElement.innerHTML === resultsArr[key] ? console.log("Passed") : console.log("Failed");
+}
+// add automatic testing
+// add option for comments on the same line
+// add function for cleaning tags through regExp pattern
+// add bootstrap
+// 
--- regicad.ts
+++ regicad.ts
@@ -6,7 +6,6 @@ class Variable {
         this.exp = exp;
     }
     get result() {
-        console.log("this.exp: ", this.exp)
         return regiRound(evaluate(this.exp), precision);
     }
 }
@@ -14,31 +13,23 @@ class Variable {
 const evaluate = (str: string) => {
     let varsNames = vars.map(x => x.name);
     str = replaceByStars(str);
-    console.log('String to evaluate: ', str); //Here need to brake string into array and check for every array element
     let strArr = str.replace(/ |;/gi, '').split(/[\+\-\**\*\/\(\)]/gi);
-    console.log("Array to evaluate: ", strArr);
     
     for (let i = 0; i <strArr.length+1; i++) {
-        
         let index = varsNames.indexOf(strArr[i]);
-        console.log("Index: ", index, "i: ", i, "srtArr.length: ", strArr.length);
-        
         if (index != -1) {
             str = str.replace(strArr[i], `(${vars[index].exp})`).replace(/ /gi, '');
             strArr = str.replace(/[\(\)]/gi, '').split(/[\+\-\**\*\/)\(\)]/gi);
-            console.log(`Expression: ${str}, strArr: ${strArr}`);
             i = -1;
         }
     }
-    console.log("String before eval: ", str, " precision: ", precision, "result: ", eval(str));
-    
     return regiRound(eval(str), precision);
 }
 
 const createVariable = (str: string) => {
     str = replaceByStars(str);
-    let name = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[0];
-    let exp = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[1];
+    let name = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[0].replace(/|(<*>)/gi, '');
+    let exp = str.replace(/ |;|&nbsp/gi, '').split('<b>=</b>')[1].replace(/|(<*>)/gi, '');
     let varsNames = vars.map(x => x.name)
     if (varsNames.includes(name)) {
         let index = varsNames.indexOf(name);
@@ -46,7 +37,6 @@ const createVariable = (str: string) => {
     } 
     let v = new Variable(name, exp);
     vars.push(v);
-    console.log(`Vars: ${vars.map(v => v.name +'=' + v.exp + '=' + v.result)}`);
     return v;
 
 }
@@ -63,18 +53,17 @@ const calculate = () => {
     for (let line in temp) {
         textElement.innerHTML +=`<div>${temp[line]}</div>`;
     }
+    save()
 }
 
 const processLine = (line:string) => {
     if (line.indexOf('=') === -1) return line
     if (line.replace(/;|&nbsp/gi, '').includes('<b>=</b>')) {
-        console.log("This is variable declaration...:", line);
         let v = createVariable(line);
         return `${v.name} <b>=</b> ${replaceStars(v.exp)}` ;
     }
     else {
         let lineArr = line.split('=');
-        console.log('This is equation, lineArr[0]:', lineArr[0]);
         return `${replaceStars(lineArr[0])} = ${evaluate(lineArr[0])}`;
     }
 }
@@ -98,10 +87,9 @@ const replaceStars = (str:string) => {
 }
 
 const save = () => {
-    let newFilename = document.getElementById('filename').value;
-    localStorage.setItem(newFilename, textElement.innerHTML);
-    if (filelist.indexOf(newFilename) ===-1) {
-        filelist.unshift(newFilename);
+    localStorage.setItem(newFilename.value, textElement.innerHTML);
+    if (filelist.indexOf(newFilename.value) ===-1) {
+        filelist.unshift(newFilename.value);
         localStorage.setItem('RegiCADfiles', filelist.join());
     }
     updateFileList();
@@ -109,12 +97,9 @@ const save = () => {
 
 const deleteFile = () => {
     let filename = selectFile.value;
-    console.log("selectFile.value: ", selectFile.value);
-    
     localStorage.removeItem(filename);
     let index = filelist.indexOf(filename);
     filelist.splice(index, 1);
-    console.log("Filelist: ", filelist);
     localStorage.removeItem('RegiCADfiles');
     localStorage.setItem('RegiCADfiles', filelist.join());
     updateFileList();
@@ -131,22 +116,27 @@ const updateFileList = () => {
 
 const loadFile = () => {
     textElement.innerHTML = localStorage.getItem(selectFile.value);
-    document.getElementById('filename').value = selectFile.value;
+    newFilename.value = selectFile.value;
 }
-let vars: Variable[] = [];
-let filelist = [];
-let regiFiles = localStorage.getItem('RegiCADfiles');
-if (regiFiles) filelist = regiFiles.split(',');
+
 const textElement = document.getElementById('content');
 const precisionEl: HTMLInputElement = document.getElementById('precision');
 const saveBtn = document.getElementById('saveBtn');
 const calcBtn = document.getElementById('calcBtn');
 const deleteBtn = document.getElementById('deleteBtn');
 let selectFile = document.getElementById('selectFile');
+let newFilename = document.getElementById('filename');
 
+let vars: Variable[] = [];
+
+let filelist = [];
+let regiFiles = localStorage.getItem('RegiCADfiles');
+if (regiFiles) filelist = regiFiles.split(',');
 let lastFile = filelist[0];
 if (lastFile) {
-    textElement.innerHTML = localStorage.getItem(lastFile);
+   textElement.innerHTML = localStorage.getItem(lastFile);
+   newFilename.value = lastFile;
+   
 }
 updateFileList();
 
@@ -159,3 +149,35 @@ precisionEl.addEventListener('change', calculate)
 saveBtn.addEventListener('click', save)
 deleteBtn.addEventListener('click', deleteFile)
 selectFile.addEventListener('click', loadFile)
+
+//Testing.....
+const testArr = [
+    "<div>1+2=</div>",
+    "<div>2*3=</div>",
+    "<div>(1+2)^3-(4*5)/6=</div>",
+    "<div>abc<b>=</b>12.3</div><div>def<b>=</b>45.6</div><div>abc+def=</div>",
+    "<div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет депрессии при открытии скважины Хазри-2 объект 4</div><div>ρ&nbsp;<b>=</b>&nbsp;1.2</div><div>Глубина верха интервала перфорации:</div><div>Интервал_верх&nbsp;<b>=</b>&nbsp;4407</div><div><br></div><div>Давление гидростатики на глубине верха интервала:</div><div>Ph&nbsp;<b>=</b>&nbsp;ρ*Интервал_верх*1.423</div><div>Ph = 7525</div><div><br></div><div>Пластовое давление:</div><div>Pf&nbsp;<b>=</b>&nbsp;47.24*145</div><div>Pf = 6850</div><div><br></div><div>После замещения необходимо стравить давление на устье для депрессии 10 МПа:</div><div>Ph - Pf + 10*145 = 2126</div><div>В МПа:</div><div>(Ph - Pf)/145 + 10 = 14.7</div><div><br></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет объема замещения</div><div>Внутренний объем НКТ на 1 м:</div><div>0.063**2*0.785 = 0</div><div>Объем вытеснения при замещении на 300 м выше IRDV:</div><div>4177-300 = 3877</div><div>3877*0.003115 = 12.1</div><div>0.5*0.55 = 0.28</div>",
+    "<div></div><div></div><div></div><div></div><div></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Volume calculations Khazri-2 zone 4</div><div>Liner_ID <b>=</b> 0.1524</div><div>Casing1_ID <b>=</b> 0.2262</div><div>Casing2_ID <b>=</b> 0.2168</div><div>Casing3_ID <b>=</b> 0.219</div><div>Packer_depth <b>=</b> 4778</div><div>Tubing_OD <b>=</b> 0.0889</div><div>Tubing_ID <b>=</b> 0.063</div><div>Well_volume <b>=</b> 0.785∙(Casing1_ID^2∙1098.19+Casing2_ID^2∙(3143.26-1098.19)+Casing3_ID^2∙(4221.24-3143.26)+Liner_ID^2∙(Packer_depth-4221.24))</div><div>Well_volume     = 170.3</div><div>Annulus_volume <b>=</b> Well_volume-0.785∙Tubing_OD^2∙Packer_depth</div><div>Annulus_volume    = 140.7</div><div>Tubing_volume <b>=</b> 0.785∙Tubing_ID^2∙Packer_depth</div><div>Tubing_volume    = 14.89</div><div><br></div>"
+
+
+]
+const resultsArr = [
+    "<div></div><div>1+2 = 3</div>",
+    "<div></div><div>2∙3 = 6</div>",
+    "<div></div><div>(1+2)^3-(4∙5)/6 = 23.67</div>",
+    "<div></div><div>abc <b>=</b> 12.3</div><div>def <b>=</b> 45.6</div><div>abc+def = 57.9</div>",
+    "<div></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет депрессии при открытии скважины Хазри-2 объект 4</div><div>ρ <b>=</b> 1.2</div><div>Глубина верха интервала перфорации:</div><div>Интервал_верх <b>=</b> 4407</div><div><br></div><div>Давление гидростатики на глубине верха интервала:</div><div>Ph <b>=</b> ρ∙Интервал_верх∙1.423</div><div>Ph  = 7525</div><div><br></div><div>Пластовое давление:</div><div>Pf <b>=</b> 47.24∙145</div><div>Pf  = 6850</div><div><br></div><div>После замещения необходимо стравить давление на устье для депрессии 10 МПа:</div><div>Ph - Pf + 10∙145  = 2126</div><div>В МПа:</div><div>(Ph - Pf)/145 + 10  = 14.66</div><div><br></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Расчет объема замещения</div><div>Внутренний объем НКТ на 1 м:</div><div>0.063^2∙0.785  = 0.003116</div><div>Объем вытеснения при замещении на 300 м выше IRDV:</div><div>4177-300  = 3877</div><div>3877∙0.003115  = 12.08</div><div>0.5∙0.55  = 0.275</div>",
+    "<div></div><div></div><div></div><div></div><div></div><div></div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Volume calculations Khazri-2 zone 4</div><div>Liner_ID <b>=</b> 0.1524</div><div>Casing1_ID <b>=</b> 0.2262</div><div>Casing2_ID <b>=</b> 0.2168</div><div>Casing3_ID <b>=</b> 0.219</div><div>Packer_depth <b>=</b> 4778</div><div>Tubing_OD <b>=</b> 0.0889</div><div>Tubing_ID <b>=</b> 0.063</div><div>Well_volume <b>=</b> 0.785∙(Casing1_ID^2∙1098.19+Casing2_ID^2∙(3143.26-1098.19)+Casing3_ID^2∙(4221.24-3143.26)+Liner_ID^2∙(Packer_depth-4221.24))</div><div>Well_volume      = 170.3</div><div>Annulus_volume <b>=</b> Well_volume-0.785∙Tubing_OD^2∙Packer_depth</div><div>Annulus_volume     = 140.7</div><div>Tubing_volume <b>=</b> 0.785∙Tubing_ID^2∙Packer_depth</div><div>Tubing_volume     = 14.89</div><div><br></div>"
+
+]
+for (let key in testArr) {
+    textElement.innerHTML = testArr[key];
+    calculate()
+    textElement.innerHTML === resultsArr[key] ? console.log("Passed"): console.log("Failed");
+}
+
+// add automatic testing
+// add option for comments on the same line
+// add function for cleaning tags through regExp pattern
+// add bootstrap
+// 
